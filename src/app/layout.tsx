952691ce3c77726825d8a4c681445e8c32be64c6
Linda Jensen <linda.jensen81@me.com>
2026-07-24 09:18:36 +0200

    Update global styles and layout padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col px-4 py-6">{children}</body>
     </html>
   );
 }
